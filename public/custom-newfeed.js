eb7833ea1ae599e46d2e86008e5884eb6f2b87d8
tuananhnvl <creatiflow.danny@gmail.com>
2024-10-15 07:57:45 +0700

    up

---
+++ public/custom-newfeed.js
@@ -0,0 +1,11 @@
+console.log('helo')
+const listh4 = document.querySelectorAll('h4')
+console.log(listh4)
+for(let i = 0; i< listh4.length;i++ ) {
+    listh4[i].style.fontSize = "1.9rem";
+    console.log('helo')
+}
+
+   
+
+
